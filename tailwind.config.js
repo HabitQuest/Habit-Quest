c7dde57fdf9cfaaf194ffb83f74fe36833b3cc57
Destiny Ross <destinyross.dev@gmail.com>
2024-06-11 20:44:58 -0400

    Configured tailwind color utilities and radial gradient for future styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,26 @@ module.exports = {
         extrabold: 800,
         black: 900,
       },
+      colors: {
+        // foreground and background colors
+        green: "#153638",
+        "dark-green": "#10292B",
+
+        // buttons and more
+        blue: "#39629B",
+        "dark-blue": "#1E4F96",
+        yellow: "#CEA24F",
+        "bright-yellow": "#FFBF00",
+
+        // task description colors
+        red: "#F0432C",
+        "bright-blue": "#3384FC",
+        green: "#24AA49",
+      },
+      backgroundImage: {
+        "yellow-gradient":
+          "radial-gradient(169.40% 89.55% at 94.76% 6.29%, #FFBF00 0%, #997300 100%)",
+      },
     },
   },
   plugins: [],
